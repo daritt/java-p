c3fa89faaf4bed2b1583b66e8af76224951f7580
daritt <darialange93@gmail.com>
2019-02-14 10:05:13 +0100

    start

---
+++ src/start/Hello.java
@@ -0,0 +1,11 @@
+package start;
+
+//komentarz
+//psvm - public static void main(String[] args)
+//sout - System.out.println()
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
