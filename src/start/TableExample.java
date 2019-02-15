7caf17cdd368949b60fc856f6d49b36b07f308c8
Daritt <darialange93@gmail.com>
2019-02-15 12:25:31 +0100

    cwi

---
+++ src/start/TableExample.java
@@ -0,0 +1,23 @@
+package start;
+
+public class TableExample {
+    public static void main(String[] args) {
+        double signal [] = {0.5, 0.11, 0.43, 0.67, 0.01, 0.11};
+        System.out.println(signal [0]);
+        System.out.println(signal [1]);
+        System.out.println(signal [2]);
+        System.out.println(signal [3]);
+        System.out.println(signal [4]);
+        System.out.println(signal [5]);
+//        modyfikacja
+        signal[4] = 0;
+        System.out.println(signal [0]);
+        System.out.println(signal [1]);
+        System.out.println(signal [2]);
+        System.out.println(signal [3]);
+        System.out.println(signal [4]);
+        System.out.println(signal [5]);
+//        długość tablicy
+     
+    }
+}
