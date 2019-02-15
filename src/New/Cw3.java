68566bf67902f4a4a2a25ef27ab5a68aa1003dfe
Daritt <darialange93@gmail.com>
2019-02-15 12:01:37 +0100

    cwi

--- src/New/Cw3.java
+++ src/New/Cw3.java
@@ -1,4 +1,13 @@
 package New;
 
 public class Cw3 {
+    public static void main(String[] args) {
+        double number = 3;
+        boolean isNormalized = true;
+        if (number > 1 || number < 0){
+            isNormalized = false;
+        }
+        System.out.println(isNormalized);
+
+    }
 }
---
+++ src/New/Cw4.java
@@ -0,0 +1,33 @@
+package New;
+
+import java.util.Scanner;
+
+public class Cw4 {
+    public static void main(String[] args) {
+        int decimalNum = 3;
+        switch (decimalNum) {
+            case 0:
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("I");
+                break;
+            case 2:
+                System.out.println("II");
+                break;
+            case 3:
+                System.out.println("III");
+                break;
+            case 4:
+                System.out.println("IV");
+                break;
+            case 5:
+                System.out.println("V");
+                break;
+            default:
+                System.out.println("Nie ma takiej liczby");
+
+        }
+
+    }
+}
